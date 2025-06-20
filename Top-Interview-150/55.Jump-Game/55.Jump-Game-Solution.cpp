bbe7350af52f6d04e436b4170f9f681be699f4bf
Jesus Soto <jhsotoglz@gmail.com>
2025-06-20 11:52:22 -0500

    Problem 55 solution added

--- Top-Interview-150/55.Jump-Game/55.Jump-Game-Solution.cpp
+++ Top-Interview-150/55.Jump-Game/55.Jump-Game-Solution.cpp
@@ -29,7 +29,12 @@ using namespace std;
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // Write your solution here!!
+        int farthest = 0;
+        for (int i = 0; i <= farthest; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (farthest >= nums.size() - 1) return true;
+        }
+        return false;
     }
 };
 
