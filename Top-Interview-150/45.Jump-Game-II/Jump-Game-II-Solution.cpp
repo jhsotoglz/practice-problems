383d0b8fc3089c864c5d5286342e7b0c3a8f9843
Jesus Soto <jhsotoglz@gmail.com>
2025-06-21 16:06:16 -0500

    Problem 45 solution completed

--- Top-Interview-150/45.Jump-Game-II/Jump-Game-II-Solution.cpp
+++ Top-Interview-150/45.Jump-Game-II/Jump-Game-II-Solution.cpp
@@ -33,7 +33,19 @@ using namespace std;
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        // Write your solution here!!!
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == currentEnd) {
+                ++jumps;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
     }
 };
 
