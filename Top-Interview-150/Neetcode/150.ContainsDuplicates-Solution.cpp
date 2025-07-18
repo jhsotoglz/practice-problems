5a5742cb5011b2588aadad6f0b5854cf6ea873cb
Jesus Soto <jhsotoglz@gmail.com>
2025-07-18 12:18:47 -0500

    Working on Neetcode problem 150 solution

---
+++ Top-Interview-150/Neetcode/150.ContainsDuplicates-Solution.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+/*
+------------------------
+Contains Duplicate
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: false
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+------------------------
+*/
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        // Write your solution here!!!
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<int> test1 = {1, 2, 3, 3};
+    vector<int> test2 = {1, 2, 3, 4};
+
+    cout << boolalpha;
+    cout << "Test 1: " << solution.hasDuplicate(test1) << endl; // true
+    cout << "Test 2: " << solution.hasDuplicate(test2) << endl; // false
+
+    return 0;
+}
