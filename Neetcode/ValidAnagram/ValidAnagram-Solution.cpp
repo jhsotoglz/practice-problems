c361040f0eacc9c0fbe703de897390bf96ad92e4
Jesus Soto <jhsotoglz@gmail.com>
2025-07-19 15:07:46 -0500

    Solution added

--- Neetcode/ValidAnagram/ValidAnagram-Solution.cpp
+++ Neetcode/ValidAnagram/ValidAnagram-Solution.cpp
@@ -1,4 +1,4 @@
-#include <unordered_map>
+
 #include <iostream>
 using namespace std;
 
@@ -26,14 +26,34 @@ Space Complexity: O(n)
 ------------------------
 */
 
-class Solution {
+class Solution
+{
 public:
-    bool isAnagram(string s, string t) {
-        // Write your solution here!!!
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+            return false;
+
+        int count[26] = {0};
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (count[i] != 0)
+                return false;
+        }
+
+        return true;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
 
     string s1 = "racecar", t1 = "carrace";
